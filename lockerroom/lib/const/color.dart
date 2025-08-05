f2536ae2e9a4e6b1e2534dc73ff1cec14cec3d71
DeokWon <khjs7878@naver.com>
2025-08-05 18:53:30 +0900

    feat: 팀 색상 상수 추가

---
+++ lockerroom/lib/const/color.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// 두산베어스
+const Doosan = Color(0xff181928);
+
+// SSG 랜더스
+const Landers = Color(0xffCE0E2D);
+
+// 한화 이글스
+const Eagles = Color(0xffFC4E00);
+
+// 키움 히어로즈
+const KIWOOM = Color(0xff570514);
+
+// 삼성 라이온즈
+const Samsung = Color(0xff074CA1);
+
+// LG 트윈스
+const LG = Color(0xffC30452);
+
+// 롯데 자이언츠
+const Lotte = Color(0xff041E42);
+
+// NC 다이노스
+const NC = Color(0xff315288);
+
+// 기아 타이거즈
+const Kia = Color(0xffEA0029);
+
+// KT 위즈
+const KT = Color(0xff000000);
