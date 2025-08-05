0429630e2d7a3e699ad9c4c21b53fb8f9198cc96
DeokWon <khjs7878@naver.com>
2025-08-05 18:40:47 +0900

    feat: 팀 모델 클래스 추가

---
+++ lockerroom/lib/model/team_model.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class TeamModel {
+  final String name;
+  final String logoPath;
+  final String symbolPath;
+  final Color color;
+
+  TeamModel({
+    required this.name,
+    required this.logoPath,
+    required this.symbolPath,
+    required this.color,
+  });
+}
