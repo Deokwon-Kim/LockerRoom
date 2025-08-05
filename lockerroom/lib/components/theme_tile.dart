6072404b3132928b36e5b5fa66cf9d8d32c06ec9
DeokWon <khjs7878@naver.com>
2025-08-05 20:06:57 +0900

    feat: 테마 타일 컴포넌트에 선택 상태 추가 및 체크 아이콘 표시 기능 구현

--- lockerroom/lib/components/theme_tile.dart
+++ lockerroom/lib/components/theme_tile.dart
@@ -4,32 +4,59 @@ import 'package:lockerroom/model/team_model.dart';
 class ThemeTile extends StatelessWidget {
   final TeamModel teamModel;
   final void Function()? onTap;
-  const ThemeTile({super.key, required this.teamModel, this.onTap});
+  final bool isSelected;
+  const ThemeTile({
+    super.key,
+    required this.teamModel,
+    this.onTap,
+    this.isSelected = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.all(15),
-            margin: EdgeInsets.symmetric(horizontal: 0),
-            decoration: BoxDecoration(
-              color: teamModel.color,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: teamModel.color),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: 110,
-                height: 110,
-                child: Image.asset(teamModel.logoPath),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(horizontal: 0),
+                decoration: BoxDecoration(
+                  color: teamModel.color,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: isSelected
+                        ? const Color.fromARGB(255, 255, 188, 2)
+                        : teamModel.color,
+                    width: 3,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 110,
+                    height: 110,
+                    child: Image.asset(teamModel.logoPath),
+                  ),
+                ),
+              ),
+
+              // 체크 아이콘 (오른쪽 위)
+            ],
+          ),
+          if (isSelected)
+            Positioned(
+              top: 8,
+              right: 8,
+              child: Icon(
+                Icons.check_circle,
+                color: const Color.fromARGB(255, 255, 188, 2),
+                size: 24,
               ),
             ),
-          ),
         ],
       ),
     );
