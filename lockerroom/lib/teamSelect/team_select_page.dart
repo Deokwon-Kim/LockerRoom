ce14f4c0edecce3b9e88bae5e269975c37648e98
DeokWon <khjs7878@naver.com>
2025-08-05 16:48:05 +0900

    feat: 팀 선택 페이지로 홈 화면 변경 및 관련 코드 정리

---
+++ lockerroom/lib/teamSelect/team_select_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TeamSelectPage extends StatelessWidget {
+  const TeamSelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
