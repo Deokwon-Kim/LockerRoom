7e11f0dff24e4df0945b5c063ffb704b9e1c9431
DeokWon <khjs7878@naver.com>
2025-08-05 18:54:40 +0900

    refactor: 팀 선택 페이지 경로 변경 및 불필요한 파일 삭제

--- lockerroom/lib/main.dart
+++ lockerroom/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lockerroom/teamSelect/team_select_page.dart';
+import 'package:lockerroom/page/team_select_page.dart';
 
 void main() {
   runApp(const MyApp());
