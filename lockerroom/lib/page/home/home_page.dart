010b87fc31a848dd3a35c8298d551322a1fd8e72
DeokWon <khjs7878@naver.com>
2025-08-11 19:59:57 +0900

    feat: 홈 페이지에 팀 모델 및 팀 제공자를 추가하여 선택된 팀에 따라 앱바 색상 변경

--- lockerroom/lib/page/home/home_page.dart
+++ lockerroom/lib/page/home/home_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lockerroom/model/team_model.dart';
+import 'package:lockerroom/provider/team_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final TeamModel teamModel;
+  const HomePage({super.key, required this.teamModel});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<TeamProvider>(
+      builder: (context, teamProvider, child) {
+        final selectedTeam = teamProvider.selectedTeam ?? teamModel;
+        return Scaffold(appBar: AppBar(backgroundColor: selectedTeam.color));
+      },
+    );
   }
 }
