60d126160c07db40ef6946956f1785a50c50208d
DeokWon <khjs7878@naver.com>
2025-08-12 18:13:45 +0900

    feat: 업로드 페이지 구현 및 이미지 선택, 텍스트 입력, 게시물 업로드 기능 추가

--- lockerroom/lib/page/home/upload_page.dart
+++ lockerroom/lib/page/home/upload_page.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:lockerroom/bottom_tab_bar/bottom_tab_bar.dart';
+import 'package:lockerroom/const/color.dart';
+import 'package:lockerroom/provider/bottom_tab_bar_provider.dart';
+import 'package:lockerroom/provider/post_provider.dart';
+import 'package:lockerroom/provider/team_provider.dart';
+import 'package:provider/provider.dart';
 
 class UploadPage extends StatelessWidget {
   const UploadPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final postProvider = Provider.of<PostProvider>(context);
+    final teamProvider = Provider.of<TeamProvider>(context);
+
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      appBar: AppBar(
+        title: Text('새 게시물', style: TextStyle(color: WHITE)),
+        backgroundColor: teamProvider.selectedTeam?.color,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(16),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: postProvider.pickImage,
+                child: Container(
+                  height: 200,
+                  width: double.infinity,
+                  color: GRAYSCALE_LABEL_300,
+                  child: postProvider.imageFile != null
+                      ? Image.file(postProvider.imageFile!, fit: BoxFit.cover)
+                      : Center(child: Text('이미지 선택')),
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                controller: postProvider.captionController,
+                decoration: InputDecoration(hintText: '내용입력...'),
+              ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  postProvider.upload();
+                  context.read<BottomTabBarProvider>().setIndex(0);
+
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => BottomTabBar()),
+                    (route) => false,
+                  );
+                },
+                child: Text('업로드'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
