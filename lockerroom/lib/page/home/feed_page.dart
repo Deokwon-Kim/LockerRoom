c1eabb36eaa8d9035b6a519b4dd65e8aef3dd4a0
DeokWon <khjs7878@naver.com>
2025-08-12 18:14:12 +0900

    feat: 피드 페이지 구현 및 게시물 목록 표시 기능 추가

--- lockerroom/lib/page/home/feed_page.dart
+++ lockerroom/lib/page/home/feed_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:lockerroom/const/color.dart';
+import 'package:lockerroom/provider/post_provider.dart';
+import 'package:provider/provider.dart';
 
 class FeedPage extends StatelessWidget {
   const FeedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final postProvider = Provider.of<PostProvider>(context);
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ...postProvider.posts.map(
+                (post) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.file(
+                      post.image,
+                      height: 300,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 8),
+                    Text(post.caption, style: TextStyle(color: Colors.black)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
