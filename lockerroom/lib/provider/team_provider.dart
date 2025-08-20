d3e85b988db7475263cf848b64dc113c5327697a
DeokWon <khjs7878@naver.com>
2025-08-20 22:45:07 +0900

    refector: 팀 모델에 symplename 필드를 추가하고, 팀 이름 검색 로직을 해당 필드를 기준으로 수정함

--- lockerroom/lib/provider/team_provider.dart
+++ lockerroom/lib/provider/team_provider.dart
@@ -23,48 +23,56 @@ class TeamProvider with ChangeNotifier {
     'team': [
       TeamModel(
         name: '두산베어스',
+        symplename: '두산',
         logoPath: 'assets/images/logo/doosan_logo.png',
         symbolPath: 'assets/images/symbol/newlogo2.png',
         color: Doosan,
       ),
       TeamModel(
         name: '삼성라이온즈',
+        symplename: '삼성',
         logoPath: 'assets/images/logo/samsung_logo.png',
         symbolPath: 'assets/images/symbol/samsung_symbol.png',
         color: Samsung,
       ),
       TeamModel(
         name: '롯데자이언츠',
+        symplename: '롯데',
         logoPath: 'assets/images/logo/lotte_logo.png',
         symbolPath: 'assets/images/symbol/lotte_symbol.png',
         color: Lotte,
       ),
       TeamModel(
         name: '기아타이거즈',
+        symplename: '기아',
         logoPath: 'assets/images/logo/kia_logo.png',
         symbolPath: 'assets/images/symbol/kia_symbol.png',
         color: Kia,
       ),
       TeamModel(
         name: 'LG트윈스',
+        symplename: 'LG',
         logoPath: 'assets/images/logo/lg_logo.png',
         symbolPath: 'assets/images/symbol/lg_symbol.png',
         color: LG,
       ),
       TeamModel(
         name: 'SSG랜더스',
+        symplename: 'SSG',
         logoPath: 'assets/images/logo/ssg_logo.png',
         symbolPath: 'assets/images/symbol/landers_symbol.png',
         color: Landers,
       ),
       TeamModel(
         name: '한화이글스',
+        symplename: '한화',
         logoPath: 'assets/images/logo/hanwha_logo.png',
         symbolPath: 'assets/images/symbol/hanwha_symbol.png',
         color: Eagles,
       ),
       TeamModel(
         name: '키움히어로즈',
+        symplename: '키움',
         logoPath: 'assets/images/logo/kiwoom_logo.png',
         symbolPath: 'assets/images/symbol/kiwoom_symbol.png',
         color: KIWOOM,
@@ -72,6 +80,7 @@ class TeamProvider with ChangeNotifier {
 
       TeamModel(
         name: 'NC다이노스',
+        symplename: 'NC',
         logoPath: 'assets/images/logo/nc_logo.png',
         symbolPath: 'assets/images/symbol/nc_symbol.png',
         color: NC,
@@ -79,6 +88,7 @@ class TeamProvider with ChangeNotifier {
 
       TeamModel(
         name: 'KT위즈',
+        symplename: 'KT',
         logoPath: 'assets/images/logo/kt_logo.png',
         symbolPath: 'assets/images/symbol/kt_symbol.png',
         color: KT,
@@ -103,7 +113,7 @@ class TeamProvider with ChangeNotifier {
   TeamModel? findTeamByName(String name) {
     final teams = _teamList['team'] ?? [];
     try {
-      return teams.firstWhere((t) => t.name == name);
+      return teams.firstWhere((t) => t.symplename == name);
     } catch (_) {
       return null;
     }
