5e42981851a135757ed38c971579fc6cd9d25fbf
DeokWon <khjs7878@naver.com>
2025-08-20 22:44:20 +0900

    feat: TeamModel에 symplename 필드 추가

--- lockerroom/lib/model/team_model.dart
+++ lockerroom/lib/model/team_model.dart
@@ -2,12 +2,14 @@ import 'dart:ui';
 
 class TeamModel {
   final String name;
+  final String symplename;
   final String logoPath;
   final String symbolPath;
   final Color color;
 
   TeamModel({
     required this.name,
+    required this.symplename,
     required this.logoPath,
     required this.symbolPath,
     required this.color,
