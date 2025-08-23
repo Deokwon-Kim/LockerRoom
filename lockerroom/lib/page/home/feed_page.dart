f72f96d0d96e7fe734448e0d9cc19be831ff1f60
DeokWon <khjs7878@naver.com>
2025-08-23 17:59:36 +0900

    feat: 게시물 클릭 시 상세 페이지로 이동하는 기능 추가 및 UI 개선

--- lockerroom/lib/page/home/feed_page.dart
+++ lockerroom/lib/page/home/feed_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:lockerroom/const/color.dart';
 import 'package:lockerroom/model/post_model.dart';
 import 'package:lockerroom/page/alert/diallog.dart';
+import 'package:lockerroom/page/home/feed_detail_page.dart';
 import 'package:lockerroom/provider/feed_provider.dart';
 import 'package:lockerroom/provider/profile_provider.dart';
 import 'package:provider/provider.dart';
@@ -116,207 +117,219 @@ class PostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final currentUserId = FirebaseAuth.instance.currentUser?.uid;
-    return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
-      child: Card(
-        color: WHITE,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FeedDetailPage(post: post)),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+        child: Card(
+          color: WHITE,
 
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // 작성자 + 프로필
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  StreamBuilder<String?>(
-                    stream: context
-                        .read<ProfileProvider>()
-                        .liveloadProfileImage(post.userId),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 작성자 + 프로필
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StreamBuilder<String?>(
+                      stream: context
+                          .read<ProfileProvider>()
+                          .liveloadProfileImage(post.userId),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return CircleAvatar(
+                            radius: 20,
+                            backgroundColor: GRAYSCALE_LABEL_300,
+                          );
+                        }
+                        if (!snapshot.hasData || snapshot.data == null) {
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: const Icon(Icons.person, size: 20),
+                          );
+                        }
                         return CircleAvatar(
                           radius: 20,
                           backgroundColor: GRAYSCALE_LABEL_300,
+                          backgroundImage: NetworkImage(snapshot.data!),
                         );
-                      }
-                      if (!snapshot.hasData || snapshot.data == null) {
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(30),
-                          child: const Icon(Icons.person, size: 20),
-                        );
-                      }
-                      return CircleAvatar(
-                        radius: 20,
-                        backgroundColor: GRAYSCALE_LABEL_300,
-                        backgroundImage: NetworkImage(snapshot.data!),
-                      );
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        post.userName,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        timeAgo(post.createdAt),
-                        style: TextStyle(
-                          color: GRAYSCALE_LABEL_500,
-                          fontSize: 13,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  currentUserId != null && post.userId == currentUserId
-                      ? PopupMenuTheme(
-                          data: PopupMenuThemeData(color: BACKGROUND_COLOR),
-                          child: PopupMenuButton<String>(
-                            icon: const Icon(Icons.more_horiz),
-                            onSelected: (value) async {
-                              if (value == 'delete') {
-                                // 삭제 확인 다이얼로그 추가
-                                showDialog(
-                                  context: context,
-                                  builder: (context) => ConfirmationDialog(
-                                    title: '삭제 확인',
-                                    content: '게시글을 삭제 하시겠습니까?',
-                                    onConfirm: () async {
-                                      await feedProvider.deletePost(post.id);
-                                      toastification.show(
-                                        context: context,
-                                        type: ToastificationType.success,
-                                        alignment: Alignment.bottomCenter,
-                                        autoCloseDuration: Duration(seconds: 2),
-                                        title: Text('게시물을 삭제했습니다'),
-                                      );
-                                    },
-                                  ),
-                                );
-                              }
-                            },
-                            itemBuilder: (context) => const [
-                              PopupMenuItem(
-                                value: 'delete',
-                                child: Text(
-                                  '삭제하기',
-                                  style: TextStyle(color: RED_DANGER_TEXT_50),
-                                ),
-                              ),
-                            ],
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          post.userName,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
                           ),
-                        )
-                      : SizedBox.shrink(),
-                ],
-              ),
-              const SizedBox(height: 8),
-              // 본문
-              Text(post.text),
-              const SizedBox(height: 8),
-
-              // 이미지/영상 슬라이드
-              if (post.mediaUrls.isNotEmpty)
-                SizedBox(
-                  height: 200,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: post.mediaUrls.length,
-                    itemBuilder: (_, i) {
-                      final url = post.mediaUrls[i];
-                      final inSingle = post.mediaUrls.length == 1;
-
-                      return Padding(
-                        padding: EdgeInsets.only(
-                          left: inSingle ? 0 : 0,
-                          right: inSingle ? 0 : 8,
                         ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: url.endsWith('.mp4')
-                              ? Container(
-                                  width: inSingle
-                                      ? 290
-                                      : 150, // 단일: 화면 전체, 여러장: 150
-                                  height: 200,
-                                  color: Colors.black12,
-                                  child: Center(child: Text('비디오 미리보기')),
-                                )
-                              : Image.network(
-                                  url,
-                                  height: 200,
-                                  width: inSingle
-                                      ? 290
-                                      : 150, // 단일: 화면 전체, 여러장: 150
-                                  fit: inSingle
-                                      ? BoxFit.cover
-                                      : BoxFit.cover, // 단일은 contain, 여러장 cover
-                                  loadingBuilder:
-                                      (context, child, loadingProgress) {
-                                        if (loadingProgress == null)
-                                          return child;
-                                        return SizedBox(
-                                          height: 200,
-                                          width: inSingle ? 290 : 150,
-                                          child: const Center(
-                                            child: CircularProgressIndicator(
-                                              color: BUTTON,
-                                            ),
+                        Text(
+                          timeAgo(post.createdAt),
+                          style: TextStyle(
+                            color: GRAYSCALE_LABEL_500,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    currentUserId != null && post.userId == currentUserId
+                        ? PopupMenuTheme(
+                            data: PopupMenuThemeData(color: BACKGROUND_COLOR),
+                            child: PopupMenuButton<String>(
+                              icon: const Icon(Icons.more_horiz),
+                              onSelected: (value) async {
+                                if (value == 'delete') {
+                                  // 삭제 확인 다이얼로그 추가
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => ConfirmationDialog(
+                                      title: '삭제 확인',
+                                      content: '게시글을 삭제 하시겠습니까?',
+                                      onConfirm: () async {
+                                        await feedProvider.deletePost(post.id);
+                                        toastification.show(
+                                          context: context,
+                                          type: ToastificationType.success,
+                                          alignment: Alignment.bottomCenter,
+                                          autoCloseDuration: Duration(
+                                            seconds: 2,
                                           ),
+                                          title: Text('게시물을 삭제했습니다'),
                                         );
                                       },
+                                    ),
+                                  );
+                                }
+                              },
+                              itemBuilder: (context) => const [
+                                PopupMenuItem(
+                                  value: 'delete',
+                                  child: Text(
+                                    '삭제하기',
+                                    style: TextStyle(color: RED_DANGER_TEXT_50),
+                                  ),
                                 ),
-                        ),
-                      );
-                    },
-                  ),
+                              ],
+                            ),
+                          )
+                        : SizedBox.shrink(),
+                  ],
                 ),
-              // 좋아요 버튼
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () => feedProvider.toggleLike(post),
-                    icon: Icon(
-                      (FirebaseAuth.instance.currentUser?.uid != null &&
-                              post.likedBy.contains(
-                                FirebaseAuth.instance.currentUser!.uid,
-                              ))
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      color:
-                          (FirebaseAuth.instance.currentUser?.uid != null &&
-                              post.likedBy.contains(
-                                FirebaseAuth.instance.currentUser!.uid,
-                              ))
-                          ? Colors.red
-                          : null,
+                const SizedBox(height: 8),
+                // 본문
+                Text(post.text),
+                const SizedBox(height: 8),
+
+                // 이미지/영상 슬라이드
+                if (post.mediaUrls.isNotEmpty)
+                  SizedBox(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: post.mediaUrls.length,
+                      itemBuilder: (_, i) {
+                        final url = post.mediaUrls[i];
+                        final inSingle = post.mediaUrls.length == 1;
+
+                        return Padding(
+                          padding: EdgeInsets.only(
+                            left: inSingle ? 0 : 0,
+                            right: inSingle ? 0 : 8,
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: url.endsWith('.mp4')
+                                ? Container(
+                                    width: inSingle
+                                        ? 290
+                                        : 150, // 단일: 화면 전체, 여러장: 150
+                                    height: 200,
+                                    color: Colors.black12,
+                                    child: Center(child: Text('비디오 미리보기')),
+                                  )
+                                : Image.network(
+                                    url,
+                                    height: 200,
+                                    width: inSingle
+                                        ? 290
+                                        : 150, // 단일: 화면 전체, 여러장: 150
+                                    fit: inSingle
+                                        ? BoxFit.cover
+                                        : BoxFit
+                                              .cover, // 단일은 contain, 여러장 cover
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                          if (loadingProgress == null)
+                                            return child;
+                                          return SizedBox(
+                                            height: 200,
+                                            width: inSingle ? 290 : 150,
+                                            child: const Center(
+                                              child: CircularProgressIndicator(
+                                                color: BUTTON,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                  ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                  Transform.translate(
-                    offset: Offset(-10, 0),
-                    child: Text('${post.likesCount}'),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(CupertinoIcons.chat_bubble),
-                  ),
-                ],
-              ),
-              Text(
-                '${post.mediaUrls.length}개의 이미지',
-                style: TextStyle(
-                  color: GRAYSCALE_LABEL_500,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w500,
+                // 좋아요 버튼
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => feedProvider.toggleLike(post),
+                      icon: Icon(
+                        (FirebaseAuth.instance.currentUser?.uid != null &&
+                                post.likedBy.contains(
+                                  FirebaseAuth.instance.currentUser!.uid,
+                                ))
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color:
+                            (FirebaseAuth.instance.currentUser?.uid != null &&
+                                post.likedBy.contains(
+                                  FirebaseAuth.instance.currentUser!.uid,
+                                ))
+                            ? Colors.red
+                            : null,
+                      ),
+                    ),
+                    Transform.translate(
+                      offset: Offset(-10, 0),
+                      child: Text('${post.likesCount}'),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(CupertinoIcons.chat_bubble),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Text(
+                  '${post.mediaUrls.length}개의 이미지',
+                  style: TextStyle(
+                    color: GRAYSCALE_LABEL_500,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
