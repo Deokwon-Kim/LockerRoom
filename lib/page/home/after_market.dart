56513a091c27d9b044972f903bd3c4e576c5d8b8
DwKim <khjs7878@naver.com>
2025-09-04 20:13:00 +0900

    feat: AfterMarket 페이지 UI 구현 및 업로드 페이지로의 네비게이션 추가

--- lib/page/home/after_market.dart
+++ lib/page/home/after_market.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:lockerroom/const/color.dart';
+import 'package:lockerroom/page/home/after_market_upload_page.dart';
 
 class AfterMarket extends StatelessWidget {
   const AfterMarket({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      appBar: AppBar(
+        backgroundColor: BACKGROUND_COLOR,
+        title: Text(
+          '더베이스 마켓',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+        scrolledUnderElevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 500),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AfterMarketUploadPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 16,
+                        ),
+                        decoration: BoxDecoration(
+                          color: BUTTON,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Icon(Icons.add, color: WHITE),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
