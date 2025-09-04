8cdcdb60ac35a362d5d218af1493071357ed8fdd
DwKim <khjs7878@naver.com>
2025-09-04 22:46:12 +0900

    feat: MarketUploadProvider를 main.dart에 추가하여 상태 관리 통합

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:lockerroom/page/setting/setting_page.dart';
 import 'package:lockerroom/page/team_select_page.dart';
 import 'package:lockerroom/provider/comment_provider.dart';
 import 'package:lockerroom/provider/feed_provider.dart';
+import 'package:lockerroom/provider/market_upload_provider.dart';
 import 'package:lockerroom/provider/profile_provider.dart';
 import 'package:lockerroom/provider/team_provider.dart';
 import 'package:lockerroom/provider/upload_provider.dart';
@@ -35,6 +36,7 @@ Future<void> main() async {
         ChangeNotifierProvider(create: (context) => ProfileProvider()),
         ChangeNotifierProvider(create: (context) => VideoProvider()),
         ChangeNotifierProvider(create: (context) => CommentProvider()),
+        ChangeNotifierProvider(create: (context) => MarketUploadProvider()),
       ],
       child: const MyApp(),
     ),
