158f99dbd65de866ae62e626f5434d9c6a66d153
DwKim <khjs7878@naver.com>
2025-09-15 21:14:47 +0900

    feat: TeamProvider에 각 팀의 경기장 정보 추가 - 팀 모델에 stadium 필드를 통해 경기장 이름을 포함

--- lib/provider/team_provider.dart
+++ lib/provider/team_provider.dart
@@ -24,6 +24,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '두산베어스',
         symplename: '두산',
+        stadium: '잠실야구장',
         logoPath: 'assets/images/logo/doosan_logo.png',
         symbolPath: 'assets/images/symbol/newlogo2.png',
         youtubeName: 'BEARS TV',
@@ -34,6 +35,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '삼성라이온즈',
         symplename: '삼성',
+        stadium: '라이온즈 파크',
         logoPath: 'assets/images/logo/samsung_logo.png',
         symbolPath: 'assets/images/symbol/samsung_symbol.png',
         youtubeName: 'LIONS TV',
@@ -44,6 +46,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '롯데자이언츠',
         symplename: '롯데',
+        stadium: '사직야구장',
         logoPath: 'assets/images/logo/lotte_logo.png',
         symbolPath: 'assets/images/symbol/lotte_symbol.png',
         youtubeName: '자이언츠 TV',
@@ -54,6 +57,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '기아타이거즈',
         symplename: '기아',
+        stadium: '챔피언스 필드',
         logoPath: 'assets/images/logo/kia_logo.png',
         symbolPath: 'assets/images/symbol/kia_symbol.png',
         youtubeName: '갸티비',
@@ -64,6 +68,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: 'LG트윈스',
         symplename: 'LG',
+        stadium: '잠실야구장',
         logoPath: 'assets/images/logo/lg_logo.png',
         symbolPath: 'assets/images/symbol/lg_symbol.png',
         youtubeName: 'LGTWINSTV',
@@ -74,6 +79,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: 'SSG랜더스',
         symplename: 'SSG',
+        stadium: '랜더스 필드',
         logoPath: 'assets/images/logo/ssg_logo.png',
         symbolPath: 'assets/images/symbol/landers_symbol.png',
         youtubeName: '쓱튜브',
@@ -84,6 +90,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '한화이글스',
         symplename: '한화',
+        stadium: '이글스 볼파크',
         logoPath: 'assets/images/logo/hanwha_logo.png',
         symbolPath: 'assets/images/symbol/hanwha_symbol.png',
         youtubeName: 'Eagles TV',
@@ -94,6 +101,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: '키움히어로즈',
         symplename: '키움',
+        stadium: '고척스카이돔',
         logoPath: 'assets/images/logo/kiwoom_logo.png',
         symbolPath: 'assets/images/symbol/kiwoom_symbol.png',
         youtubeName: '큠튜브',
@@ -105,6 +113,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: 'NC다이노스',
         symplename: 'NC',
+        stadium: '창원 NC파크',
         logoPath: 'assets/images/logo/nc_logo.png',
         symbolPath: 'assets/images/symbol/nc_symbol.png',
         youtubeName: '엔튜브',
@@ -116,6 +125,7 @@ class TeamProvider with ChangeNotifier {
       TeamModel(
         name: 'KT위즈',
         symplename: 'KT',
+        stadium: '위즈파크',
         logoPath: 'assets/images/logo/kt_logo.png',
         symbolPath: 'assets/images/symbol/kt_symbol.png',
         youtubeName: 'kt wiz - 위즈TV',
