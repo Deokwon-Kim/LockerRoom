8e8ae199ddcbb93bc3ca3ec5b37e307214b83e37
DwKim <khjs7878@naver.com>
2025-09-29 22:16:48 +0900

    feat: 사용자 모델에 팔로워 수 및 팔로잉 수 필드 추가

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -5,12 +5,16 @@ class UserModel {
   final String useremail;
   final String uid;
   final String? profileImageUrl;
+  final int followersCount;
+  final int followingCount;
 
   UserModel({
     required this.username,
     required this.useremail,
     required this.uid,
     this.profileImageUrl,
+    required this.followersCount,
+    required this.followingCount,
   });
 
   factory UserModel.fromDoc(DocumentSnapshot doc) {
@@ -20,6 +24,8 @@ class UserModel {
       useremail: data['email'] ?? 'Unknown',
       uid: data['uid'],
       profileImageUrl: data['profileImageUrl'] ?? '',
+      followersCount: data['followersCount'] ?? 0,
+      followingCount: data['followingCount'] ?? 0,
     );
   }
 }
