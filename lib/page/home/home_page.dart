eba8b1fa2997e2e375479de8783f5428885448ee
DwKim <khjs7878@naver.com>
2025-12-03 22:53:58 +0900

    refector: 더보기 버튼 arrow 아이콘 추가 및 퀴즈 페이지 네비게이션 quiz_tabBar로 수정

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lockerroom/bottom_tab_bar/quiz_tab_bar.dart';
 import 'package:lockerroom/const/color.dart';
 import 'package:lockerroom/model/team_model.dart';
 import 'package:lockerroom/page/feed/feed_detail_page.dart';
@@ -18,7 +19,6 @@ import 'package:lockerroom/page/food_store/ncParkStore_page.dart';
 import 'package:lockerroom/page/food_store/wizParkStore_page.dart';
 import 'package:lockerroom/page/intution_record/intution_record_list_page.dart';
 import 'package:lockerroom/page/intution_record/intution_record_upload_page.dart';
-import 'package:lockerroom/page/quiz/quiz_start_page.dart';
 import 'package:lockerroom/page/schedule/schedule.dart';
 import 'package:lockerroom/provider/block_provider.dart';
 import 'package:lockerroom/provider/feed_provider.dart';
@@ -334,9 +334,7 @@ class _HomePageState extends State<HomePage> {
                     onTap: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(
-                          builder: (context) => QuizStartPage(),
-                        ),
+                        MaterialPageRoute(builder: (context) => QuizTabBar()),
                       );
                     },
                     child: Row(
