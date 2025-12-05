a8d7ab562170f592618885f5719d43261466db2d
DwKim <khjs7878@naver.com>
2025-12-05 21:40:52 +0900

    feat: 퀴즈 카테고리별 순위 추가

--- lib/page/quiz/quiz_ranking_page.dart
+++ lib/page/quiz/quiz_ranking_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lockerroom/const/color.dart';
 import 'package:lockerroom/model/ranking_user_model.dart';
@@ -13,6 +14,17 @@ class QuizRankingPage extends StatefulWidget {
 }
 
 class _QuizRankingPageState extends State<QuizRankingPage> {
+  // 카테고리 목록
+  final List<Map<String, String>> _categories = [
+    {'value': 'all', 'label': '전체'},
+    {'value': 'KBO역사', 'label': 'KBO역사'},
+    {'value': '야구룰', 'label': '야구룰'},
+    {'value': '선수퀴즈', 'label': '선수퀴즈'},
+    {'value': '기록', 'label': '기록'},
+    {'value': '응원가', 'label': '응원가'},
+    {'value': '랜덤', 'label': '랜덤'},
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -20,6 +32,82 @@ class _QuizRankingPageState extends State<QuizRankingPage> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<QuizRankingProvider>().fetchRankings();
     });
+  }
+
+  void _showCategoryPicker(BuildContext context, QuizRankingProvider qrp) {
+    int selectedIndex = _categories.indexWhere(
+      (c) => c['value'] == qrp.selectedCategory,
+    );
+    if (selectedIndex == -1) selectedIndex = 0;
+
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 250,
+          color: CupertinoColors.systemBackground.resolveFrom(context),
+          child: Column(
+            children: [
+              // 상단 바
+              Container(
+                height: 44,
+                decoration: BoxDecoration(
+                  color: CupertinoColors.systemBackground.resolveFrom(context),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: CupertinoColors.separator.resolveFrom(context),
+                      width: 0.5,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CupertinoButton(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text('취소', style: TextStyle(color: BUTTON)),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    CupertinoButton(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text('완료', style: TextStyle(color: BUTTON)),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: CupertinoPicker(
+                  scrollController: FixedExtentScrollController(
+                    initialItem: selectedIndex,
+                  ),
+                  onSelectedItemChanged: (int index) {
+                    qrp.setCategory(_categories[index]['value']!);
+                  },
+                  itemExtent: 40,
+                  children: _categories.map((category) {
+                    return Center(
+                      child: Text(
+                        category['label']!,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  String _getCategoryLabel(String value) {
+    final category = _categories.firstWhere(
+      (c) => c['value'] == value,
+      orElse: () => {'value': 'all', 'label': '전체'},
+    );
+    return category['label']!;
   }
 
   @override
@@ -31,15 +119,48 @@ class _QuizRankingPageState extends State<QuizRankingPage> {
       appBar: AppBar(
         scrolledUnderElevation: 0,
         backgroundColor: Colors.grey[100],
-        title: Text(
-          '퀴즈 랭킹',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'kbo',
-          ),
+        title: Consumer<QuizRankingProvider>(
+          builder: (context, qrp, child) {
+            return Row(
+              children: [
+                Text(
+                  '퀴즈 랭킹',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'kbo',
+                  ),
+                ),
+                SizedBox(width: 12),
+                GestureDetector(
+                  onTap: () => _showCategoryPicker(context, qrp),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: WHITE,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: GRAYSCALE_LABEL_300),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          _getCategoryLabel(qrp.selectedCategory),
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Icon(Icons.keyboard_arrow_down, size: 20),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
-        centerTitle: true,
+        centerTitle: false,
         elevation: 0,
         actions: [
           IconButton(
