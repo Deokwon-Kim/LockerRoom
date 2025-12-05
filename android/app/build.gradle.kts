a8e42688ae929d197bdba0f153a4e848252db6e0
DwKim <khjs7878@naver.com>
2025-12-05 22:00:03 +0900

    versionCode 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 125
+        versionCode = 126
         versionName = "1.2.0"
     }
 
