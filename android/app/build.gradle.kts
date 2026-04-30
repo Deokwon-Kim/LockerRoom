c454ce544a78181e53b7bbbd88e28fa8261a8f29
DwKim <khjs7878@naver.com>
2026-04-30 11:47:14 +0900

    버전명 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 143
-        versionName = "1.3.6"
+        versionCode = 146
+        versionName = "1.4.0"
     }
 
     signingConfigs {
